dfda1e82d4dcda7d29a437db43645b7c8f948449
HenriqueA7 <riqueaus@gmail.com>
2024-09-24 17:19:56 -0300

    Versão 1.0

---
+++ Profiles/FilmeProfile.cs
@@ -0,0 +1,16 @@
+﻿using FilmesAPI.Data.DTO;
+using FilmesAPI.Models;
+using AutoMapper;
+
+namespace FilmesAPI.Profiles;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile()
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+}
